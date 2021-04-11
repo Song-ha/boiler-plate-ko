ec3c0831527ef5f535b49706107e1c40bbbb263f
songha <thdgk6304@naver.com>
2021-04-11 14:38:59 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://songha:kk28326304@boilerplate.ifyr3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕')
+  res.send('Hello World!')
+})
+
+app.post('/register', (req,res) => {
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+  user.save( (err,userInfo) => {
+    if (res.status(200)) 
+      return res.json({success: true})
+    else
+      return res.json({success: fail })
+  })
 })
 
 app.listen(port, () => {
